b22ebb48549e064e2b32d00f8b225ceb94623624
charlie.andrews-jubelt <candrewsjubelt@gmail.com>
2022-03-15 20:50:16 -0700

    refactor: using argparse instead of input() to solicit arguments

--- email_filter.py
+++ email_filter.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 from typing import Iterable, Dict, List
 
@@ -18,38 +19,37 @@ def filter_rows(
 
 
 def main():
-    input_file_name = input("Enter input tsv file: ")
-    output_file_name = input("Enter output txt file: ")
-    keywords_name = input("Enter keywords to remove from name list csv file: ")
-    keywords_email = input("Enter keywords to remove from email list csv file: ")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, help='input TSV file path', required=True)
+    parser.add_argument('--output', type=str, help='output file path', default='./data/output.txt')
+    parser.add_argument('--name_keywords', type=str, help='File with name keywords to remove',
+                        default='./data/KeywordName.csv')
+    parser.add_argument('--email_keywords', type=str, help='File with email keywords to remove',
+                        default='./data/KeywordEmail.csv')
+    args = parser.parse_args()
 
-    with open(keywords_name, newline='') as f:
+    with open(args.name_keywords, newline='') as f:
         reader = csv.reader(f)
-        keyword_list = list(reader)
+        keyword_list_name = [row[0] for row in list(reader)]
 
-    keyword_list_name = [row[0] for row in keyword_list]
-
-    with open(keywords_email, newline='') as f:
+    with open(args.email_keywords, newline='') as f:
         reader = csv.reader(f)
-        keyword_list = list(reader)
+        keyword_list_email = [row[0] for row in list(reader)]
 
-    keyword_list_email = [row[0] for row in keyword_list]
-
-    with open(input_file_name, 'rU', encoding="utf8") as input_file:
+    with open(args.input, 'rU', encoding="utf8") as input_file:
         dict_reader = csv.DictReader(input_file, delimiter='\t')
-        with open(output_file_name, "w+") as output_file:
+        with open(args.output, "w+") as output_file:
             if FIRST_NAME_COL_NAME not in dict_reader.fieldnames:
-                raise Exception(f'Missing required column: ${FIRST_NAME_COL_NAME}')
+                raise Exception(f'Missing required column: {FIRST_NAME_COL_NAME}')
             if EMAIL_COL_NAME not in dict_reader.fieldnames:
-                raise Exception(f'Missing required column: ${EMAIL_COL_NAME}')
+                raise Exception(f'Missing required column: {EMAIL_COL_NAME}')
             include_rows = filter_rows(dict_reader, name_keywords=keyword_list_name, email_keywords=keyword_list_email)
-            col_names_ordered = sorted(include_rows[0].keys())
             for row in include_rows:
-                f.write(OUTPUT_COL_DELIMITER.join([
-                    row[col] for col in col_names_ordered
+                output_file.write(OUTPUT_COL_DELIMITER.join([
+                    row[col] or '' for col in dict_reader.fieldnames
                 ]) + "\n")
 
-    print(f'Wrote ${len(include_rows)} rows to ${output_file_name}')
+    print(f'Wrote {len(include_rows)} rows to {args.output}')
 
 
 if __name__ == '__main__':
